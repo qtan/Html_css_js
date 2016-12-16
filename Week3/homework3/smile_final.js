31e8e0bddadfe8f6278ef702323fad408fde469e
Qi Tan <qtan@ualberta.ca>
2016-12-15 23:16:42 -0700

    分开

--- Week3/homework3/smile_final.js
+++ Week3/homework3/smile_final.js
@@ -3,44 +3,48 @@ var theLeftSide = document.getElementById("leftSide");
 var theRightSide = document.getElementById("rightSide");
 var theBody = document.getElementsByTagName("body")[0];
 // after page loaded
-window.onload = function () {
-  generateFaces();
-  cloneFace();
 
-  theLeftSide.lastChild.onclick = nextLevel;
-  theBody.onclick = gameOver;
-}
+
+
 function nextLevel(event) {
-  event.stopPropagation();
-  numberOfFaces += 5;
-  while (theLeftSide.firstChild)
-    theLeftSide.removeChild(theLeftSide.firstChild);
-  while (theRightSide.firstChild)
-    theRightSide.removeChild(theRightSide.firstChild);
-  generateFaces();
-  cloneFace();
-  theLeftSide.lastChild.onclick = nextLevel;
+    event.stopPropagation();
+    numberOfFaces += 5;
+    while (theLeftSide.firstChild)
+        theLeftSide.removeChild(theLeftSide.firstChild);
+    while (theRightSide.firstChild)
+        theRightSide.removeChild(theRightSide.firstChild);
+    generateFaces();
 }
+
 function gameOver() {
-  alert("Game Over!");
-  theBody.onclick = null;
-  theLeftSide.lastChild.onclick = null;
+    alert("Game Over!");
+    theBody.onclick = null;
+    theLeftSide.lastChild.onclick = null;
 }
 
 function generateFaces() {
-  for (var i = 0; i < numberOfFaces; i++) {
-    var img = document.createElement("img");
-    img.src = "smile.png";
-    img.style.top = getRandNum() + "px";
-    img.style.left = getRandNum() + "px";
-    theLeftSide.appendChild(img);
-  }
+    for (var i = 0; i < numberOfFaces; i++) {
+        var img = document.createElement("img");
+        img.src = "smile.png";
+        img.style.top = getRandNum() + "px";
+        img.style.left = getRandNum() + "px";
+        theLeftSide.appendChild(img);
+    }
+    cloneFace();
+    theLeftSide.lastChild.onclick = function() {
+        nextLevel(event)
+    };
+    theBody.onclick = function() {
+        gameOver()
+    };
 }
+
 function cloneFace() {
-  var leftSideImages = theLeftSide.cloneNode(true);
-  leftSideImages.removeChild(leftSideImages.lastChild);
-  theRightSide.appendChild(leftSideImages);
+    var leftSideImages = theLeftSide.cloneNode(true);
+    leftSideImages.removeChild(leftSideImages.lastChild);
+    theRightSide.appendChild(leftSideImages);
 }
+
 function getRandNum() {
-  return Math.floor(Math.random() * 400);
-// }
+    return Math.floor(Math.random() * 400);
+}
